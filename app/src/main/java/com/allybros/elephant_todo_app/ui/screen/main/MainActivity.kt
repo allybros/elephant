e969ac0fa15cbc0b67ddb12a7d21f5a825ed16e1
Orcun Kamiloglu <orcunkamiloglu@gmail.com>
2022-07-10 19:22:28 +0300

    MainPage impl.

--- app/src/main/java/com/allybros/elephant_todo_app/ui/screen/main/MainActivity.kt
+++ app/src/main/java/com/allybros/elephant_todo_app/ui/screen/main/MainActivity.kt
@@ -1,25 +1,27 @@
 package com.allybros.elephant_todo_app.ui.screen.main
 
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.RequiresApi
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.allybros.elephant_todo_app.ui.screen.add.AddDialog
 import com.allybros.elephant_todo_app.ui.theme.Elephant_todo_appTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -48,16 +50,58 @@ fun MainScreen() {
             showDialog.value = it
         }, "10/10/2022")
 
-    Column(
-        Modifier
+    Surface(
+        modifier = Modifier
             .fillMaxWidth()
-            .wrapContentHeight()) {
-            Text(
-                text = "hrell",
-                Modifier
-                    .clickable{ showDialog.value = true },
-                fontSize = 124.sp
+            .fillMaxHeight(),
+        color = Color.White
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+        ){
+            BoxWithConstraints(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth()
+            ) {
+                ConstraintLayout {
+                    val (appBar, list, addNewButton) = createRefs()
+                    Box{
+                        AddNew { showDialog.value = true }
+                    }
+                }
+            }
+        }
+
+    }
+
+}
+
+@Composable
+fun AddNew(onClicked: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .wrapContentWidth()
+            .wrapContentHeight(),
+        contentAlignment = Alignment.Center
+    ) {
+        OutlinedButton(
+            onClick = { onClicked.invoke() },
+            border = null
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = "New",
+                modifier = Modifier
+                    .size(36.dp)
             )
+            Text(
+                text = "Add New",
+                fontSize = 36.sp
+            )
+        }
     }
 }
 
